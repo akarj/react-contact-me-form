4fa3660c2c8ee545041021d236c56e2d51783bad
Anuj Kumar <kumar_191851@student.nitw.ac.in>
2021-11-11 21:55:46 +0530

    validation rules were setuped...

--- src/components/formInput/FormInput.jsx
+++ src/components/formInput/FormInput.jsx
@@ -1,17 +1,28 @@
+import { useState } from "react";
 import "./FormInput.scss";
 
 const FormInput = props => {
-  const { label, onChange, id } = props;
+  const [focused, setFocused] = useState(false);
+  const { label, onChange, id, type } = props;
+
+  const handleFocus = e => {
+    setFocused(true);
+  };
+
   return (
     <div className="formInput">
       <label htmlFor={props.name}>{label}</label>
       <input
-        type="text"
+        type={type}
         name={props.name}
         id={id}
         placeholder={props.placeholder}
         onChange={onChange}
+        required={props.required}
+        onBlur={handleFocus}
+        focused={focused.toString()}
       />
+      <span>{props.errorMessage}</span>
     </div>
   );
 };
